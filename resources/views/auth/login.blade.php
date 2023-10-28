11f6a4fb539aaf628e357f0eb18ce76067741986
Motoki Suzuki <moto7szk@gmial.com>
2023-10-28 18:18:38 +0900

    Change design of login card.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row justify-content-center mt-5 mb-5">
+        <div class="col-md-6">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header text-center" style="background-color: #D9D9D9;">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -39,7 +39,8 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
+                        <!-- Remember me -->
+                        {{-- <div class="row mb-3">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
@@ -49,20 +50,20 @@
                                     </label>
                                 </div>
                             </div>
+                        </div> --}}
+
+                        <div class="row mb-3 justify-content-center">
+                            <button type="submit" class="btn" style="background-color: #D9D9D9; width:170px;">
+                                    {{ __('Login') }}
+                            </button>
                         </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
+                        <div class="row mb-2">
+                            @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
-                                @endif
-                            </div>
+                            @endif
                         </div>
                     </form>
                 </div>
