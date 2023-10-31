5413ebd82f2562f68ee3a5abd25b786aafc53299
kayo-24 <kyoa_mintcafe@yahoo.co.jp>
2023-10-31 21:19:13 +0900

    Create restraunts show page

---
+++ resources/views/restaurants/show.blade.php
@@ -0,0 +1,128 @@
+
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="h1">SHOWING</div>
+
+    <div class="row">
+        <div class="col-2">
+            <div class="card border" style="width: 549px;">
+                <div class="card-header p-0">
+                    <p class="bg-warning m-0" style="height: 402px; width: 549px;">image here</p>
+                </div>
+                <div class="card-body w-100 border">
+                    <div class="row">
+                        <div class="col-6 ">
+                            <p class="h3">Restaurant name</p>
+                                {{-- @if($post->user->avatar) --}}
+                                    {{-- <img src="{{ $post->user->avatar }}" alt="{{ $post->user->name }}" class="rounded-circle avatar-sm"> --}}
+                                {{-- @else
+                                    <i class="fa-solid fa-circle-user text-secondary icon-sm"></i>
+                                @endif --}}
+
+                        </div>
+                        <div class="col-6 text-end h3">
+                            <a href="" class="text-decoration-none text-dark">
+                                Rating
+                                {{-- {{ $post->user->name }} --}}
+                            </a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <p class="h4">Place here</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <p class="h4">eatures here</p>
+                        </div>
+                        <div class="col-6 text-end" style="font-size: 16px;">
+                            Price here
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-2">
+            <div class="card border" style="width: 549px;">
+                <div class="card-header p-0">
+                    <p class="bg-warning m-0" style="height: 402px; width: 549px;">image here</p>
+                </div>
+                <div class="card-body w-100 border">
+                    <div class="row">
+                        <div class="col-6 ">
+                            <p class="h3">Restaurant name</p>
+                                {{-- @if($post->user->avatar) --}}
+                                    {{-- <img src="{{ $post->user->avatar }}" alt="{{ $post->user->name }}" class="rounded-circle avatar-sm"> --}}
+                                {{-- @else
+                                    <i class="fa-solid fa-circle-user text-secondary icon-sm"></i>
+                                @endif --}}
+
+                        </div>
+                        <div class="col-6 text-end h3">
+                            <a href="" class="text-decoration-none text-dark">
+                                Rating
+                                {{-- {{ $post->user->name }} --}}
+                            </a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <p class="h4">Place here</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <p class="h4">eatures here</p>
+                        </div>
+                        <div class="col-6 text-end" style="font-size: 16px;">
+                            Price here
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-2">
+            <div class="card border" style="width: 549px;">
+                <div class="card-header p-0">
+                    <p class="bg-warning m-0" style="height: 402px; width: 549px;">image here</p>
+                </div>
+                <div class="card-body w-100 border">
+                    <div class="row">
+                        <div class="col-6 ">
+                            <p class="h3">Restaurant name</p>
+                                {{-- @if($post->user->avatar) --}}
+                                    {{-- <img src="{{ $post->user->avatar }}" alt="{{ $post->user->name }}" class="rounded-circle avatar-sm"> --}}
+                                {{-- @else
+                                    <i class="fa-solid fa-circle-user text-secondary icon-sm"></i>
+                                @endif --}}
+
+                        </div>
+                        <div class="col-6 text-end h3">
+                            <a href="" class="text-decoration-none text-dark">
+                                Rating
+                                {{-- {{ $post->user->name }} --}}
+                            </a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <p class="h4">Place here</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <p class="h4">eatures here</p>
+                        </div>
+                        <div class="col-6 text-end" style="font-size: 16px;">
+                            Price here
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
