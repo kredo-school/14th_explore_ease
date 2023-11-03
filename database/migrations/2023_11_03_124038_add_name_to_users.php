d3153e840e361493da72636e1880e207494b1cfe
yonbo <yonbot0172@gmail.com>
2023-11-03 21:47:27 +0900

    【feature/DBmigration】add migration file to fix register page

--- database/migrations/2023_11_03_124038_add_name_to_users.php
+++ database/migrations/2023_11_03_124038_add_name_to_users.php
@@ -12,9 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('name');  //Column削除
-            $table->dropColumn('email_verified_at');  //Column削除
-            $table->dropColumn('remember_token');  //Column削除
+            $table->string('name');
         });
     }
 
@@ -24,9 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('name');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->rememberToken();
+            $table->dropColumn('name');  //Column削除
         });
     }
 };
