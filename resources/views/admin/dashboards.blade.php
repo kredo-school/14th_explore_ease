c4a21b3b0d00f923dbd3816f994d2344032cf1ac
Chie Tomita <tmtmcctmtm@gmail.com>
2023-11-04 16:26:49 +0900

    deleate the comment "TEST" from log.txt

---
+++ resources/views/admin/dashboards.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+
+{{-- temporaly use for front end coding--}}
+               <!-- Bootstrap -->
+               <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+               <!-- FontAwsome CDN -->
+               <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+{{-- end --}}
+
+<h2>Dashboards</h2>
+{{-- Number of users, reviews, and reservations --}}
+<i class="fa-solid fa-circle-user fs-5"></i>
+<i class="fa-solid fa-house-user fs-5"></i>
+<i class="fa-solid fa-utensils fs-5"></i>
+<i class="fa-solid fa-clipboard fs-5"></i>
+<i class="fa-solid fa-calendar-check fs-5"></i>
+
+
+{{-- Monthly reports --}}
+
+
+{{-- Nationality --}}
+
+
+@endsection
