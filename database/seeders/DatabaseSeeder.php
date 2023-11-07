e4bf978c6866a16f080d65bd40c47f24ad5ea558
yonbo <yonbot0172@gmail.com>
2023-11-07 21:14:54 +0900

    【feature/DBmigration】add countrycd to nationarity

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserTypeSeeder::class,
             AreaTypeSeeder::class,
+            NationaritySeeder::class,
         ]);
     }
 }
