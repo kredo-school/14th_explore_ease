4bb1023801c144d2d23ca2e2d456c2ebe90c2789
kayo-24 <kyoa_mintcafe@yahoo.co.jp>
2023-11-09 22:16:34 +0900

    I created

--- resources/views/restaurant/edit.blade.php
+++ resources/views/restaurant/edit.blade.php
@@ -25,7 +25,7 @@
             </div>
             <div class="mb-3">
                 <label for="restaurant_address" class="form-label h3">Restaurant address</label>
-                <input type="text" id="restaurant_address" class="form-control" placeholder="Restaurant address">
+                <input type="text" id="restaurant_address" class="form-control" placeholder="">
             </div>
             <div class="mb-3">
                 <label for="restaurant_location" class="form-label h3">Please place the pin accurately at your outlet's location on the map</label>
