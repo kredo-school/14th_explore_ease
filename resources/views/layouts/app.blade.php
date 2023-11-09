87ce09b16322f1cd9968a9bea6342eb231c8df11
kayo-24 <kyoa_mintcafe@yahoo.co.jp>
2023-11-09 20:19:53 +0900

    I checked the problems

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,34 +9,7 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    
-    <!-- for photo uploading design -->
-    <!-- <meta charset="utf-8">
-	<meta name="viewport" content="width=device-width">
-  <script src="https://use.fontawesome.com/3a2eaf6206.js"></script>
-	<title>repl.it</title>
-  <style id="photo.upload">
-
-      div{
-        text-align:center;
-        padding:3%;
-        border:thin solid black;
-      }
-      
-      input[type="file"]{
-        display: none;
-      }
-
-      label{
-        cursor:pointer;
-      }
-      
-      #imageName{
-        color:green;
-      }
-  </style> -->
-
-    <!-- Fonts -->
+        <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
