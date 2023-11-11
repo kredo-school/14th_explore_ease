0c80751c24ce2f1f2c80a7c8a5ff27ec5acf328b
Motoki Suzuki <moto7szk@gmial.com>
2023-11-11 18:19:09 +0900

    [feature/Ranking] edit slideshow retry

--- resources/views/users/menu.blade.php
+++
@@ -1,9 +0,0 @@
-
-
-
-    <div class="list-group">
-        <a href="#" class="list-group-item"> Profile </a>
-        <a href="#" class="list-group-item"> Reservation </a>
-        <a href="#" class="list-group-item"> Reviews </a>
-        <a href="#" class="list-group-item"> Bookmarks </a>
-    </div>
