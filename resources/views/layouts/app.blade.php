4e2f65d938afcba28f487c65961b37324cc961e6
Chie Tomita <tmtmcctmtm@gmail.com>
2023-11-11 11:23:02 +0900

    【Featurer_admin_dashbord_all_users】fixed style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,10 +21,10 @@
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     @yield('styles')
 
-    {{-- Styles --}}
+    <!-- Styles -->
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
 
-    {{-- Font awesome --}}
+    <!-- Font awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 </head>
