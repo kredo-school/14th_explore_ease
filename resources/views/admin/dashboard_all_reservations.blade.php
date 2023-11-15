14a5b6965376f0206c3afb463537823763683250
kayo-24 <kyoa_mintcafe@yahoo.co.jp>
2023-11-15 20:43:27 +0900

    I fixed

--- resources/views/admin/dashboard_all_reservations.blade.php
+++ resources/views/admin/dashboard_all_reservations.blade.php
@@ -2,13 +2,6 @@
 
 @section('content')
 
-{{-- temporaly use for front end coding--}}
-               <!-- Bootstrap -->
-               <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-               <!-- FontAwsome CDN -->
-               <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-{{-- end --}}
-
 <div class="container w-75">
     <h2>All Reservations</h2>
     {{-- Search Box --}}
