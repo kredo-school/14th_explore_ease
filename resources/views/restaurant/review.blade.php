2cee6e9bd7bad00af1734f872f13b79fdc33dff4
Motoki Suzuki <moto7szk@gmial.com>
2023-11-18 17:58:36 +0900

    [feature/restaurant_review] move <style> to from blade to style.css

--- resources/views/restaurant/review.blade.php
+++ resources/views/restaurant/review.blade.php
@@ -3,32 +3,6 @@
 @section('title', 'restaurants.detail')
 
 @section('content')
-<style>
-.rate-form {
-  display: flex;
-  flex-direction: row-reverse;
-  justify-content: flex-end;
-}
-.rate-form input[type=radio] {
-  display: none;
-}
-.rate-form label {
-  position: relative;
-  padding: 0 5px;
-  color: #ccc;
-  cursor: pointer;
-  font-size: 50px;
-}
-.rate-form label:hover {
-  color: #ffcc00;
-}
-.rate-form label:hover ~ label {
-  color: #ffcc00;
-}
-.rate-form input[type=radio]:checked ~ label {
-  color: #ffcc00;
-}
-</style>
 
 @section('styles')
 <!-- Chart.js -->
