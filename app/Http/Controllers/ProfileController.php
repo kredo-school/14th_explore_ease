56cae8f5c97276f4e74b34eaa0d87edc02fca9aa
kayo-24 <kyoa_mintcafe@yahoo.co.jp>
2023-11-29 19:44:35 +0900

    done

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Bookmark;
-use App\Models\Profile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Profile;
+use App\Models\User;
+use App\Models\Nationarity;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\RestaurantPhoto;
 
 class ProfileController extends Controller
 {   
