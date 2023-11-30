e9e12226b7c9feacc317a118b4a728d7097ac8a0
kayo-24 <kyoa_mintcafe@yahoo.co.jp>
2023-11-30 19:55:36 +0900

    modified

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -138,7 +138,7 @@ class RestaurantController extends Controller
         }
 
         $seat = new Seat();
-        if($request->seat = "available"){
+        if($request->seat == "available"){
             $seat->restaurant_id = $restaurant->id;
             $seat->reservation_minutes = 60;
             $seat->save();
@@ -148,11 +148,11 @@ class RestaurantController extends Controller
             if($request->{"photo_".$i+1}){
                 $restaurant_photo = new RestaurantPhoto();
                 $restaurant_photo->restaurant_id = $restaurant->id;
-                if($i = 0){
+                if($i == 0){
                     $restaurant_photo->name = "First photo";
-                } elseif($i = 1){
+                } elseif($i == 1){
                     $restaurant_photo->name = "Second photo";
-                } elseif($i = 2){
+                } elseif($i == 2){
                     $restaurant_photo->name = "Third photo";
                 }
                 $restaurant_photo->photo = 'data:image/' . $request->{"photo_".$i+1}->extension().
@@ -169,13 +169,13 @@ class RestaurantController extends Controller
                 $budget->restaurant_id = $restaurant->id;
                 $budget->timezonetype = "1";
                 $budget->budgetindex = $i+1;
-                if($i = 0){
+                if($i == 0){
                     $budget->budgetvalue = "￥";
-                } elseif ($i=1){
+                } elseif ($i == 1){
                     $budget->budgetvalue = "￥￥";
-                } elseif ($i=2){
+                } elseif ($i == 2){
                     $budget->budgetvalue = "￥￥￥";
-                } elseif ($i=3){
+                } elseif ($i == 3){
                     $budget->budgetvalue = "￥￥￥￥";
                 }
 
@@ -190,13 +190,13 @@ class RestaurantController extends Controller
                 $budget->restaurant_id = $restaurant->id;
                 $budget->timezonetype = "2";
                 $budget->budgetindex = $i+1;
-                if($i = 0){
+                if($i == 0){
                     $budget->budgetvalue = "￥";
-                } elseif ($i=1){
+                } elseif ($i == 1){
                     $budget->budgetvalue = "￥￥";
-                } elseif ($i=2){
+                } elseif ($i == 2){
                     $budget->budgetvalue = "￥￥￥";
-                } elseif ($i=3){
+                } elseif ($i == 3){
                     $budget->budgetvalue = "￥￥￥￥";
                 }
 
