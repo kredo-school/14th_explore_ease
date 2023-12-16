0ff8c12f76998f920075164d6728ac0c90a3c0e9
DESKTOP-2511SMF\kazuya <kazuyakuse@gmail.com>
2023-12-16 16:28:03 +0900

    [feature/profile] add backend of menu bar

--- resources/views/users/menu.blade.php
+++ resources/views/users/menu.blade.php
@@ -2,8 +2,8 @@
         <a href="{{ route('profile.show', Auth::user()->id) }}" class="list-group-item"> Profile </a>
         {{-- Users can display Reservation, Review and Bookmarks but owners and admins can't display them --}}
         @if(Auth::user()->profile->usertype_id == 2)
-            <a href="{{ route('profile.reservation') }}" class="list-group-item"> Reservation </a>
-            <a href="#" class="list-group-item"> Reviews </a>
+            <a href="{{ route('reservation.show', Auth::user()->id) }}" class="list-group-item"> Reservation </a>
+            <a href="{{ route('review.show', Auth::user()->id) }}" class="list-group-item"> Reviews </a>
             <a href="{{ route('bookmark.show', Auth::user()->id) }}" class="list-group-item"> Bookmarks </a>
         @endif
     </div>
