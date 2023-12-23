e74a86e7318f1a56cdb2381c8a4f3914bb037bf0
Motoki Suzuki <moto7szk@gmial.com>
2023-12-23 19:43:26 +0900

    [feature/admin_dashboard_all_reservations] create modal

--- lang/en/messages.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-  'search_copy1' => 'Discover and book excerent restaurants',
-  'search_copy2' => 'selected by the Explore-Ease Guide',
-  'search_placeholder' => 'Search for Destination, restaurant...',
-];
--- lang/ja/messages.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-  'search_copy1' => 'Explore-Easeが選んだレストランを',
-  'search_copy2' => '検索・予約する',
-  'search_placeholder' => 'レストラン、エリアなどで検索',
-];
