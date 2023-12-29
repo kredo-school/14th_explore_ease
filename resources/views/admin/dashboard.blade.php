0a0c0925f114123d89b78900893158b03ca061f7
yonbo <yonbot0172@gmail.com>
2023-12-29 17:03:07 +0900

    【feature/middleware】to avoid https check of chrome on heroku

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,7 +6,7 @@
 
 <!-- Chart.js for graph -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 @vite(['resources/js/dashboard_graph.js'])
 
 
